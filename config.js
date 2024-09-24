687569d4f57477a347cce57864ba405e0c62213b
vantiep9654 <chidautay345@gmail.com>
2024-09-24 17:31:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/profile.php?id=100083992609476'
+    messLink: 'https://www.facebook.com/100083992609476'
 }
